bcb8c45bc678946dda78a425d1a1b510dc71c395
Sahil-jagdale <130088119+Sahil-jagdale@users.noreply.github.com>
2025-07-17 13:31:46 +0530

    Update newRequest.jsx

--- FRONTEND/src/utils/newRequest.jsx
+++ FRONTEND/src/utils/newRequest.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequests = axios.create({
-    baseURL: "http://localhost:3000/api/",
+    baseURL: "https://explore-mate.onrender.com/api/",
     withCredentials: true,
 });
 
